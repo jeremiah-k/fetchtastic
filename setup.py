397f4d455870197a1d3a92fd4d33d0b6a3f4d536
Jeremiah K <17190268+jeremiah-k@users.noreply.github.com>
2024-10-12 08:03:19 -0500

    Add __init__.py, adjust setup files

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
 from setuptools import setup, find_packages
 
-setup()
+setup(
+    name='fetchtastic',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pick',
+        'PyYAML',
+        'urllib3',
+    ],
+    entry_points={
+        'console_scripts': [
+            'fetchtastic=app.cli:main',
+        ],
+    },
+    # Include other metadata as needed
+)
